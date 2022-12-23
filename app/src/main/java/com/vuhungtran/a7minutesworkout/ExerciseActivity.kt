fed2772f497eeb915710099d77eac7aa6d2258b9
hungtv <tranvuhung@outlook.com>
2022-12-23 15:13:00 +0700

    add countdown timer

--- app/src/main/java/com/vuhungtran/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/vuhungtran/a7minutesworkout/ExerciseActivity.kt
@@ -2,9 +2,18 @@ package com.vuhungtran.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.vuhungtran.a7minutesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
+    //Step 1 - START
+    //Variable for Rest Timer and later on we will initialize it.
+    private var resetTime: CountDownTimer? = null
+    // Variable for timer progress. As initial value the rest progress is set to 0. As we are about to start.
+    private var restProgress = 0
+    //END
+
     private var binding: ActivityExerciseBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +28,56 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.toolbarExercise?.setOnClickListener {
             onBackPressed()
         }
+
+        //Step: 4 - Calling the function to make it visible on screen.)-->
+        //START
+        setupRestView() // REST View is set in this function
+        //END
+    }
+
+    //STEP 2:
+    private fun setRestProgressBar(){
+        binding?.progressBar?.progress = restProgress
+
+        resetTime = object : CountDownTimer(10000, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                binding?.progressBar?.progress = 10 - restProgress
+                binding?.tvTimer?.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "Here now we will start the exercise.",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+
+        }.start()
+    }
+
+    //STEP 3: Setting up get ready view
+    //START
+    /**
+     * Function is used to set the timer for REST.
+     */
+    private fun setupRestView(){
+        if(resetTime != null){
+            resetTime!!.cancel()
+            restProgress = 0
+        }
+        // This function is used to set the progress details.
+        setRestProgressBar()
+    }
+
+    override fun onDestroy() {
+        if(resetTime != null){
+            resetTime?.cancel()
+            restProgress = 0
+        }
+        super.onDestroy()
+        binding = null
     }
 }
